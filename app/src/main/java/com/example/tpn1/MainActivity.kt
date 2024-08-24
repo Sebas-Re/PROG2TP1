e34b18bdab07d999fd80e017dd30935f8fe57bf1
Manu <manupais22@gmail.com>
2024-08-24 00:32:02 -0300

    Correciones de vistas preview y esteticas

--- app/src/main/java/com/example/tpn1/MainActivity.kt
+++ app/src/main/java/com/example/tpn1/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.tpn1
 
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
@@ -34,15 +37,22 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.compose.ui.unit.sp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -69,7 +79,7 @@ fun MiApp() {
 fun PantallaPrincipal_Header() {
     TopAppBar(
         title = { Text("TP Nº1") },
-        colors = topAppBarColors(containerColor = Color.Cyan)
+        colors = topAppBarColors(containerColor = Color.Blue)
 
     )
 }
@@ -78,12 +88,26 @@ fun PantallaPrincipal_Header() {
 @Composable
 fun PantallaEjercicio1Header() {
     TopAppBar(
-        title = { Text("Ejercicio 1") },
-        colors = topAppBarColors(containerColor = Color.Cyan)
-
+        title = {
+            Text(
+                text = "Suma",
+                color = Color.White,
+                modifier = Modifier
+                    .background(Color.Blue)
+                    .padding(16.dp),
+                textAlign = TextAlign.Center
+            )
+        },
+        colors = topAppBarColors(containerColor = Color.Blue)
     )
 }
 
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun PantallaPrincipalPreview() {
+    val navController = rememberNavController() // NavController Dummy
+    PantallaPrincipal(navController)
+}
 
 @Suppress("PreviewAnnotationInFunctionWithParameters")
 @Preview(showBackground = true, showSystemUi = true)
@@ -124,48 +148,61 @@ fun PantallaPrincipal(navController: NavController) {
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun PantallaEjercicio1() {
-    Scaffold(topBar = { PantallaEjercicio1Header() }) { padding ->
+      Scaffold(topBar = { PantallaEjercicio1Header() }) { padding ->
         Column(modifier = Modifier
             .padding(padding)
             .fillMaxSize()
+            .padding(top = 40.dp)
         ) {
             Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceAround  // distribuye los elementos uniformemente
+                verticalAlignment = Alignment.CenterVertically,  // Alinear verticalmente al centro
+                //horizontalArrangement = Arrangement.Start,       // Alinear horizontalmente al inicio
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
             ) {
-                TextFieldNumeros(texto = "Primer Numero")
-                TextFieldNumeros(texto = "Segundo Numero")
+                // TextView
+                AndroidView(
+                    factory = { context ->
+                        TextView(context).apply {
+                            text = "Primer Número:"  // Establece el texto
+                            textSize = 20f  // Establece el tamaño del texto
+                        }
+                    },
+                    modifier = Modifier
+                        //.weight(1f)
+                        .padding(top = 4.dp)
+                )
+
+                // EditText (solo números)
+                AndroidView(
+                    factory = { context ->
+                        EditText(context).apply {
+                            //hint = "Número"
+                            inputType = android.text.InputType.TYPE_CLASS_NUMBER  // Solo números
+                            textSize = 20f
+
+                        }
+                    },
+                    modifier = Modifier.width(250.dp).padding(20.dp)
+                )
             }
-
-            Spacer(modifier = Modifier.height(19.dp))
-
+        }
             Button(
                 onClick = { /* Handle button click */ },
-                modifier = Modifier.align(Alignment.CenterHorizontally) // centrar boton
+                //modifier = Modifier.align(Alignment.CenterHorizontally) // centrar boton
             ) {
                 Text("Calcular")
             }
 
-            Spacer(modifier = Modifier.weight(1f))
+            //Spacer(modifier = Modifier.weight(1f))
 
             Text(
                 text = "INTEGRANTES: Sebastián Re, Cristian Benitez, Manuel Pais, Adriel Lopez, Dario Troilo",
                 modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
+                    //.align(Alignment.CenterHorizontally)
                     .padding(16.dp)
             )
         }
     }
-}
 
-
-@Composable
-fun TextFieldNumeros(texto: String) {
-    var text by remember { mutableStateOf("") }
-
-    OutlinedTextField(
-        value = text,
-        onValueChange = { text = it },
-        label = { Text(texto) }
-    )
-}
